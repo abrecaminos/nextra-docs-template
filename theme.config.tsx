11eff05e03d52f1a4cad91082fccbe81e28879fa
abrecaminos <43588992+abrecaminos@users.noreply.github.com>
2024-06-19 20:24:16 -0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <img src="https://ghost.org/images/logos/ghost-logo-dark.png" alt=""/>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© 2024 Ghost',
   },
   faviconGlyph: '/favicon.ico'
 }
